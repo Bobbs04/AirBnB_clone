98e5ea2d32c0e419e14f62234bd7b1f187facdf4
kelvin <edithmukoro3@gmail.com>
2023-02-12 18:52:04 +0100

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-"""
-Defines amenities
-"""
+
 from models.base_model import BaseModel
 
-
 class Amenity(BaseModel):
-    """Defines amenities that user can choose from to offer at its place"""
+    """Amenity class inherits from BaseModel
+
+    Attribute:
+        name (str): Public class attribute for Amenity's name
+    """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """init method for Amenity class
+
+        Attributes:
+            args (list): The list with arguments
+            kwargs (dict): A dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
+
