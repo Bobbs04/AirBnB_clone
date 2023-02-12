ad321f04e432709fec00230430f10ab576a3464c
kelvin <edithmukoro3@gmail.com>
2023-02-12 19:04:51 +0100

    update

--- models/city.py
+++ models/city.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-"""
-Defines city
-"""
+
 from models.base_model import BaseModel
 
-
 class City(BaseModel):
-    """defines city to look for"""
-    state_id = ""
+    """City class inherits from BaseModel
+
+    Attributes:
+        name (str): Public class attribute for City's name
+        state_id (str): Public class attribute for City's state_id
+    """
     name = ""
+    state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """init method for City class
+
+        Attributes:
+            args (list): The list with arguments
+            kwargs (dict): A dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
+
--- models/review.py
+++ models/review.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
-"""
-Defines review class
-"""
+
 from models.base_model import BaseModel
 
-
 class Review(BaseModel):
-    """Reviews made by users about a place"""
+    """Review class inherits from BaseModel
+
+    Attributes:
+        place_id (str): Public class attribute for Review's place_id
+        user_id (str): Public class attribute for Review's user_id
+        text (str): Public class attribute for Review's text
+    """
     place_id = ""
     user_id = ""
     text = ""
+
+    def __init__(self, *args, **kwargs):
+        """init method for Review class
+
+        Attributes:
+            args (list): The list with arguments
+            kwargs (dict): A dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
+
